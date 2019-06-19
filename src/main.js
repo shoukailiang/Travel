cb78384cbba698c57fff69a44f429427f329803d
shoukailiang <shoukailiang@qq.com>
2019-06-19 15:50:05 +0800

    finish home

--- src/main.js
+++ src/main.js
@@ -1,16 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import store from './store'
 // 解决移动端300ms延迟问题
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
+// 取了别名在css中要加~，在js中不用
+import 'styles/reset.css'
 // 解决移动端1px边框问题
-import './assets/styles/border.css'
-import './assets/styles/iconfont.css'
-
+import 'styles/border.css'
+import 'styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 Vue.config.productionTip = false
 fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 new Vue({
   router,
   store,
